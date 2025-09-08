bd4fd2f241a591b661b0d5847e0c916dc0e9b4fa
Gorilla-Mode <tobias_olsen02@hotmail.com>
2025-09-08 17:16:13 +0200

    inital commit

---
+++ NRLObstacleReporting.UnitTests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using NRLObstacleReporting.Controllers;
+using Xunit;
+
+namespace NRLObstacleReporting.UnitTests.Controllers;
+
+[TestSubject(typeof(HomeController))]
+public class HomeControllerTest
+{
+
+    [Fact]
+    public void METHOD()
+    {
+        
+    }
+}
