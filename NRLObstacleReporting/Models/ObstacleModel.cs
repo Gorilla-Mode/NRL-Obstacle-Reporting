d76c254c9d2f36f96c0f3251ba8e4cacac352a6b
sivert-svanes <siverss@uia.no>
2025-09-22 16:36:54 +0200

    feat: added complete ObstacleModel

--- NRLObstacleReporting/Models/ObstacleModel.cs
+++ NRLObstacleReporting/Models/ObstacleModel.cs
@@ -3,16 +3,24 @@ using System.ComponentModel.DataAnnotations;
 
 public class ObstacleModel
 {
+    
+    [MaxLength(100)]
+    public string ObstacleName { get; set; }
+
+   
+    [MaxLength(100)]
+    public string ObstacleDescription { get; set; }
+
     [Required(ErrorMessage = "This field is required")]
-    [Range(0,150)]
+    [Range(0, 150)]
     public int ObstacleHeightMeter { get; set; }
 
-    [MaxLength(200)] [Required(ErrorMessage = "This field is required")]
+    [MaxLength(200)] 
     public string? ObstacleCoordinates { get; set; }
 
     [Required(ErrorMessage = "This field is required")]
     [MaxLength(200)]
     public string? ObstacleType { get; set; }
     
-    public string? ObstacleIlluminated { get; set; }
+    public bool? ObstacleIlluminated { get; set; }
 }
