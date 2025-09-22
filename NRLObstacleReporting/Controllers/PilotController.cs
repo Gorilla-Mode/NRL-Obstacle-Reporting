aa94f731f34200d59681e33243498216968e2cc0
sivert-svanes <siverss@uia.no>
2025-09-22 16:46:07 +0200

    feat: Added pilot view and controller

---
+++ NRLObstacleReporting/Controllers/PilotController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NRLObstacleReporting.Models;
+using System.Diagnostics;
+
+namespace NRLObstacleReporting.Controllers
+{
+    public class PilotController : Controller
+    {
+        public IActionResult PilotIndex()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult View_Reports_Pilot()
+        {
+            return View("View_Reports_Pilot");
+        }
+
+    }
+}
