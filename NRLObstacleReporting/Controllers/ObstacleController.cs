77d7606e94c44f523db664b3befab65144eb63a0
sivert-svanes <siverss@uia.no>
2025-09-22 16:36:13 +0200

    feat: added ObstacleController

--- NRLObstacleReporting/Controllers/ObstacleController.cs
+++ NRLObstacleReporting/Controllers/ObstacleController.cs
@@ -1,12 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NRLObstacleReporting.db;
+using NRLObstacleReporting.Models;
+using System.Collections.Generic;
 
-namespace NRLObstacleReporting.Controllers
+
+namespace Prototype_test.Controllers
 {
     public class ObstacleController : Controller
     {
-        public IActionResult Index()
+
+
+        [HttpGet]
+        public ActionResult Dataform_1()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Dataform_1(ObstacleModel obstaclemodel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(obstaclemodel);
+            }
+
+            return View("Dataform_2", obstaclemodel);
+        }
+
+        [HttpPost]
+        public ActionResult Dataform_2(ObstacleModel obstaclemodel)
+        {
+            return View("Dataform_3", obstaclemodel);
+        }
+
+        [HttpPost]
+        public ActionResult Dataform_3(ObstacleModel obstaclemodel)
+        {
+            //if (!ModelState.IsValid)
+            //{
+            //    return View(obstaclemodel);
+            //}
+
+            Localdatabase.AddObstacle(obstaclemodel);
+
+            return View("Overview", obstaclemodel);
         }
     }
 }
