36978af14de9230f76d935935fffee4826dc36b0
Gorilla-Mode <tobias_olsen02@hotmail.com>
2025-09-22 12:59:22 +0200

    feat: added database as static list

--- NRLObstacleReporting/db/Localdatabase.cs
+++ NRLObstacleReporting/db/Localdatabase.cs
@@ -2,5 +2,16 @@
 
 public class Localdatabase
 {
-    
+    private static List<Models.ObstacleModel> _obstacles = new List<Models.ObstacleModel>();
+
+    public static void AddObstacle(Models.ObstacleModel obstacleModel)
+    {
+        _obstacles.Add(obstacleModel);
+        Console.WriteLine($"Obstacle {obstacleModel.GetHashCode()} added to database");
+    }
+
+    public static List<Models.ObstacleModel> GetDatabase()
+    {
+        return _obstacles;
+    }
 }
