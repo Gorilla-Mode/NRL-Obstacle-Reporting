bc2da2f15114f1754edfea8b324ac9b4adbc279e
Gorilla-Mode <tobias_olsen02@hotmail.com>
2025-09-22 12:41:36 +0200

    feat: added obstacle model

---
+++ NRLObstacleReporting/Models/ObstacleModel.cs
@@ -0,0 +1,18 @@
+﻿namespace NRLObstacleReporting.Models;
+using System.ComponentModel.DataAnnotations;
+
+public class ObstacleModel
+{
+    [Required(ErrorMessage = "This field is required")]
+    [Range(0,150)]
+    public int ObstacleHeightMeter { get; set; }
+
+    [MaxLength(200)] [Required(ErrorMessage = "This field is required")]
+    public string? ObstacleCoordinates { get; set; }
+
+    [Required(ErrorMessage = "This field is required")]
+    [MaxLength(200)]
+    public string? ObstacleType { get; set; }
+    
+    public string? ObstacleIlluminated { get; set; }
+}
