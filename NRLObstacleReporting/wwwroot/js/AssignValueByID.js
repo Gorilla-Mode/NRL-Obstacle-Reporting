47700b220bb2d6e338f4ac835104dea4bcbc2ee3
Gorilla-Mode <tobias_olsen02@hotmail.com>
2025-10-03 03:08:01 +0200

    chore: moved dom scripts to seperate js file

---
+++ NRLObstacleReporting/wwwroot/js/AssignValueByID.js
@@ -0,0 +1,7 @@
+﻿//for your own sanity make sure the input into the function is absolutely correct, shit made me lose my mind with 
+//useless fucking type errors, yeah, thanks for letting me know a string is undefined "GET REAL" - David Lynch, 2008 
+function AssignValueByID (ButtonID, InputID, Value){
+    document.getElementById(ButtonID).addEventListener("click", function() {
+        document.getElementById(InputID).value = Value;
+    });
+}
