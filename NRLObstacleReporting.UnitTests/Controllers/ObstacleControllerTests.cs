0110ca722962e954d18ced649db9d79c6f613e7e
Gorilla-Mode <tobias_olsen02@hotmail.com>
2025-10-06 22:43:58 +0200

    test: test for invalid model state in dataform 1,2 and 3

--- NRLObstacleReporting.UnitTests/Controllers/ObstacleControllerTests.cs
+++ NRLObstacleReporting.UnitTests/Controllers/ObstacleControllerTests.cs
@@ -8,34 +8,77 @@ namespace NRLObstacleReporting.UnitTests.Controllers;
 [TestSubject(typeof(HomeController))]
 public class ObstacleControllerTests
 {
+    private static ObstacleController InstanitateObstacleController()
+    {
+        var controller = new ObstacleController();
+        return controller;
+    }
     [Fact]
     public void DataformStep1ReturnsDataformStep1View()
     {
-        var controller = new ObstacleController();
+        var controller = InstanitateObstacleController();
 
         var result = controller.DataformStep1();
         var viewResult = Assert.IsType<ViewResult>(result);
 
         Assert.Equal(null, viewResult.ViewName);
     }
+    
     [Fact]
     public void DataformStep2ReturnsDataformStep2View()
     {
-        var controller = new ObstacleController();
+        var controller = InstanitateObstacleController();
 
         var result = controller.DataformStep2();
         var viewResult = Assert.IsType<ViewResult>(result);
 
         Assert.Equal(null, viewResult.ViewName);
     }
+    
     [Fact]
     public void DataformStep3ReturnsOverviewView()
     {
-        var controller = new ObstacleController();
+        var controller = InstanitateObstacleController();
 
         var result = controller.DataformStep3();
         var viewResult = Assert.IsType<ViewResult>(result);
 
+        Assert.Equal(null, viewResult.ViewName);
+    }
+
+    [Fact]
+    public void DataFormStep1InvalidModelStateReturnsDataformStep1View()
+    {
+        var controller = InstanitateObstacleController();
+        controller.ModelState.AddModelError("ObstacleHeightMeter", "Obstacle height meter is required.");
+        
+        var result = controller.DataformStep1();
+        var viewResult = Assert.IsType<ViewResult>(result);
+        
+        Assert.Equal(null, viewResult.ViewName);
+    }
+    
+    [Fact]
+    public void DataFormStep2InvalidModelStateReturnsDataformStep2View()
+    {
+        var controller = InstanitateObstacleController();
+        controller.ModelState.AddModelError("ObstacleHeightMeter", "Obstacle height meter is required.");
+        
+        var result = controller.DataformStep2();
+        var viewResult = Assert.IsType<ViewResult>(result);
+        
+        Assert.Equal(null, viewResult.ViewName);
+    }
+    
+    [Fact]
+    public void DataFormStep3InvalidModelStateReturnsDataformStep3View()
+    {
+        var controller = InstanitateObstacleController();
+        controller.ModelState.AddModelError("ObstacleHeightMeter", "Obstacle height meter is required.");
+        
+        var result = controller.DataformStep3();
+        var viewResult = Assert.IsType<ViewResult>(result);
+        
         Assert.Equal(null, viewResult.ViewName);
     }
 }
