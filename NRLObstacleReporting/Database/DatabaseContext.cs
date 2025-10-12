3c4666dee18ceb94f0a2a35c122665438e32f3d3
iverkroken <iverkroken@gmail.com>
2025-10-12 16:58:36 +0200

    build: created a context file for the database

---
+++ NRLObstacleReporting/Database/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NRLObstacleReporting.Database;
+
+public class DatabaseContext : DbContext
+{
+     public DbSet<ObstacleCompleteModel> ObstacleCompleteModel { get; set; }
+
+     public DatabaseContext()
+     {
+     }
+     
+     public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+     {
+          
+     }
+     
+     
+}
+
