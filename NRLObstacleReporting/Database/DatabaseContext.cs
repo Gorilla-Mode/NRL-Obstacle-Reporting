b97f19b24c63b0c48ec78e076814e635e5c07173
iverkroken <iverkroken@gmail.com>
2025-10-13 19:31:25 +0200

    build: added modelbuilders

--- NRLObstacleReporting/Database/DatabaseContext.cs
+++ NRLObstacleReporting/Database/DatabaseContext.cs
@@ -4,7 +4,9 @@ namespace NRLObstacleReporting.Database;
 
 public class DatabaseContext : DbContext
 {
-     public DbSet<ObstacleCompleteModel> ObstacleCompleteModel { get; set; } = null!;
+     public DbSet<Obstacle> Obstacle { get; set; } = null!;
+     public DbSet<Pilot> Pilot { get; set; } = null!;
+     public DbSet<Registrar> Registrar { get; set; } = null!;
      //public DbSet<TableClass>  TableClass { get; set; } = null!;
      public DatabaseContext()
      {
@@ -17,18 +19,35 @@ public class DatabaseContext : DbContext
      
      protected override void OnModelCreating(ModelBuilder modelBuilder)
      {
-          modelBuilder.Entity<ObstacleCompleteModel>(entity =>
+          modelBuilder.Entity<Obstacle>(entity =>
           {
                entity.HasKey(e => e.ObstacleId);
                entity.HasKey(e => e.IsDraft);
+               entity.Property(e => e.ObstacleType);
                entity.Property(e => e.ObstacleName);
                entity.Property(e => e.ObstacleDescription);
                entity.Property(e => e.ObstacleIlluminated);
                entity.Property(e => e.GeometryGeoJson);
                entity.Property(e => e.ObstacleHeightMeter);
-
           });
+
+          modelBuilder.Entity<Pilot>(entity =>
+          {
+               entity.HasKey(e => e.PilotId);
+               entity.Property(e => e.Name);
+               entity.Property(e => e.Organization);
+          });
+          
+          modelBuilder.Entity<Registrar>(entity =>
+          {
+               entity.HasKey(e => e.RegistrarId);
+               entity.Property(e => e.Name);
+               entity.Property(e => e.Organization);
+               entity.Property(e => e.Role);
+          });
+          
           base.OnModelCreating(modelBuilder);
+          
      }
      
 }
