4e6b4dd9d2e4219f44391bb079f2a0b5a7765b9e
iverkroken <iverkroken@gmail.com>
2025-10-13 19:30:09 +0200

    build: added pilot datamodel

---
+++ NRLObstacleReporting/Database/Pilot.cs
@@ -0,0 +1,8 @@
+﻿namespace NRLObstacleReporting.Database;
+
+public class Pilot
+{
+    public int PilotId { get; set; }  //(PK)
+    public string? Organization { get; set; }
+    public string? Name { get; set; }
+}
