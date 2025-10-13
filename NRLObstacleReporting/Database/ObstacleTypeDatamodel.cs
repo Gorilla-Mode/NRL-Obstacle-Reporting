795a4f45c365ae7c0c19677aff02f032f8916ea5
iverkroken <iverkroken@gmail.com>
2025-10-13 20:24:30 +0200

    build: added ObstacleTypeDatamodel.cs

---
+++ NRLObstacleReporting/Database/ObstacleTypeDatamodel.cs
@@ -0,0 +1,7 @@
+﻿namespace NRLObstacleReporting.Database;
+
+public class ObstacleTypeDatamodel
+{
+    public int ObstacleTypeId { get; set; }  //PK
+    public string? ObstacleName { get; set; }
+}
