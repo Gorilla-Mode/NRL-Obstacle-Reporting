54cf1afd5b8f7cfb1f306c59ad95071a875cc884
iverkroken <iverkroken@gmail.com>
2025-10-13 20:24:10 +0200

    build: added ReportDatamodel.cs

---
+++ NRLObstacleReporting/Database/ReportDatamodel.cs
@@ -0,0 +1,11 @@
+﻿namespace NRLObstacleReporting.Database;
+
+public class ReportDatamodel
+{
+    public int ReportId { get; set; }        
+    public DateTime Dato { get; set; }
+    public string? Status { get; set; } 
+    
+    public int PilotId { get; set; }
+    public PilotDatamodel? Pilot { get; set; } // navigasjon
+}
