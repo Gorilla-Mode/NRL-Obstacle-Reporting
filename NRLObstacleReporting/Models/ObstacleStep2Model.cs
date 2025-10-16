e8687a67ace45e4a370c97e67c9308f44734499d
sivert-svanes <siverss@uia.no>
2025-10-16 16:07:56 +0200

    build: coordinates / geojson data is required

--- NRLObstacleReporting/Models/ObstacleStep2Model.cs
+++ NRLObstacleReporting/Models/ObstacleStep2Model.cs
@@ -1,9 +1,12 @@
-﻿namespace NRLObstacleReporting.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NRLObstacleReporting.Models;
 
 public class ObstacleStep2Model
 {
     public int ObstacleId { get; set; }
     public bool IsDraft { get; set; } = true;
     public bool SaveDraft { get; set; }
+    [Required]
     public string? GeometryGeoJson { get; set; }
 }
