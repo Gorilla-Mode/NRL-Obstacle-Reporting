f5b78d2e03578ef438cb325401db33c5dda552cb
sivert-svanes <siverss@uia.no>
2025-10-16 17:40:34 +0200

    feat: created new js file to generate a simple map with layer

---
+++ NRLObstacleReporting/wwwroot/js/MapScripts/GenerateMap.js
@@ -0,0 +1,16 @@
+﻿/**
+ * 
+ * @returns a map to use
+ * @
+ */
+function GenerateMap()
+{
+     var map = L.map('map').setView([58.14671, 7.9956], 13);
+
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    
+    return map;
+}
