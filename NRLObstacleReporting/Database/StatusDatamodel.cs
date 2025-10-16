eb02c1ba30717b362d36d7a268460b96788841be
iverkroken <iverkroken@gmail.com>
2025-10-16 13:31:33 +0200

    build: added reports as a list in StatusDatamodel.cs

--- NRLObstacleReporting/Database/StatusDatamodel.cs
+++ NRLObstacleReporting/Database/StatusDatamodel.cs
@@ -4,4 +4,6 @@ public class StatusDatamodel
 {
     public int StatusId { get; set; }
     public string StatusName { get; set; } = null!;
+    
+    public ICollection<ReportDatamodel> Reports { get; set; } = new List<ReportDatamodel>();
 }
