d936c108b9e2344c8f1e0658e1975de52a91b019
Gorilla-Mode <tobias_olsen02@hotmail.com>
2025-10-17 00:27:12 +0200

    docs: added doc string

--- NRLObstacleReporting/StartupTests/IStartupDatabaseTest.cs
+++ NRLObstacleReporting/StartupTests/IStartupDatabaseTest.cs
@@ -1,6 +1,8 @@
 ﻿namespace NRLObstacleReporting.StartupTests;
-
-public interface IStartupTest
+/// <summary>
+/// To be implemented by startup database test. Used to run tests from subclasses polymorphically.
+/// </summary>
+public interface IStartupDatabaseTest
 {
     //method should run all test methods in a class
     public void InvokeAllTests();
