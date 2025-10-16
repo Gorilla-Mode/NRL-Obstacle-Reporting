499dcf4896eda3990af6bc340800c45f447d9dc7
Gorilla-Mode <tobias_olsen02@hotmail.com>
2025-10-16 02:30:44 +0200

    build: added jetbrains annotations to suppress dumb method not used warnings, guess what bitch it is used

---
+++ NRLObstacleReporting/StartupTests/InternalDatabaseConnectionTest.cs
@@ -0,0 +1,171 @@
+﻿using System.Data;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using JetBrains.Annotations;
+using MySqlConnector;
+using Xunit;
+
+namespace NRLObstacleReporting.StartupTests;
+/// <summary>
+/// Collection of tests for the database
+/// <param name="config">used to get connection string from appsettings</param>
+/// </summary>
+public class DatabaseTest(IConfiguration config)
+{
+    /*
+     * IMPORTANT
+     * When adding more tests to this class, make sure to use the "testprefix" for any new tests. Do this so the
+     * testinternalconnection method can collect the method and invoke it. Make sure to  SuccessMessage as return
+     * value if the test didn't fail, the testinternal.
+     */
+    
+    private readonly MySqlConnection _internalConnection = new MySqlConnection(config.GetConnectionString("InternalConnection"));
+    
+    /// <summary>
+    /// Used to mark a test as a method to run
+    /// </summary>
+    private const string TestPrefix = "Check";
+    
+    /// <summary>
+    /// Success return value of each test. Checked against to represent result as bool
+    /// </summary>
+    private const string SuccessMessage = "Success"; 
+    
+    /// <summary>
+    /// Runs all internal database connection tests in the class.
+    /// </summary>
+    public void TestInternalConnection()
+    {
+        //contains result of each test as bool, used to count fails/successes
+        List<bool> testResults = new List<bool>();
+        
+        Console.WriteLine("|-------------------------------------------");
+        Console.WriteLine("| Running all internal connection tests:");
+        
+        //collects all private instanced methods with testprefix into array
+        MethodInfo[] databaseConnectionTests = GetType()
+            .GetMethods(BindingFlags.NonPublic | BindingFlags.Instance)
+            .Where(test => test.Name.StartsWith(TestPrefix))
+            .ToArray();
+        
+        for (int i = 0; i < databaseConnectionTests.Count(); i++)
+        {
+            Console.WriteLine("|-------------------------------------------");
+            
+            //Formatting. Finds uppercase letters and adds a space.
+            string testName = Regex.Replace(databaseConnectionTests[i].Name, @"([a-z])([A-Z])", "$1 $2");
+            //Runs a given test
+            //Formatting. Asserts in xunit spit out multiline strings, adds "|" at start of each new line.
+            string testResult = databaseConnectionTests[i].Invoke(this, new object[0])!.ToString()!.ReplaceLineEndings("\n| ");
+            
+            //console result output
+            Console.WriteLine($"| {i + 1}. {testName}: {testResult}");
+            
+            switch (testResult)
+            {
+                case SuccessMessage:
+                    testResults.Add(true);
+                    break;
+                default:
+                    testResults.Add(false);
+                    break;
+            }
+            
+            if (testResults.Count() == databaseConnectionTests.Count())
+            {
+                int failedTests = testResults.Count(fail => fail == false);
+                int successfulTests = testResults.Count(success => success);
+                
+                Console.WriteLine("|-------------------------------------------");
+                Console.WriteLine($"| Tests succeeded: {successfulTests}");
+                Console.WriteLine($"| Tests failed: {failedTests}");
+                Console.WriteLine("|-------------------------------------------");
+            }
+        }
+    }
+    
+    [UsedImplicitly]
+    private string CheckConnectionOpens()
+    {
+        try
+        {
+            _internalConnection.Open();
+            return SuccessMessage;
+        }
+        catch (Exception e)
+        {
+            return e.Message;
+        }
+    }
+    
+    [UsedImplicitly]
+    private string CheckConnectionState()
+    {
+        try
+        {
+            Assert.True(_internalConnection.State == ConnectionState.Open);
+            return SuccessMessage;
+        }
+        catch (Exception e)
+        {
+            return $"{e.Message}";
+        }
+    }
+    
+    [UsedImplicitly]
+    private string CheckConnectionDatasource()
+    {
+        try
+        {
+            Assert.True(_internalConnection.DataSource == "db");
+            return SuccessMessage;
+        }
+        catch (Exception e)
+        {
+            return e.Message;
+        }
+    }
+    
+    [UsedImplicitly]
+    private string CheckConnectionDatabase()
+    {
+        try
+        {
+            Assert.True(_internalConnection.Database == "nrl");
+            return SuccessMessage;
+        }
+        catch (Exception e)
+        {
+            return e.Message;
+        }
+    }
+    
+    [UsedImplicitly]
+    private string CheckConnectionPing()
+    {
+        try
+        {
+            Assert.True(_internalConnection.Ping());
+            return SuccessMessage;
+        }
+        catch (Exception e)
+        {
+            return e.Message;
+        }
+    }
+    
+    [UsedImplicitly]
+    private string CheckConnectionClose()
+    {
+        try
+        {
+            _internalConnection.Close();
+            return SuccessMessage;
+        }
+        catch (Exception e)
+        {
+            return e.Message;
+        }
+    }
+    
+}
