da99632a19ca662e72d110c233abb6fa44d2877c
iverkroken <iverkroken@gmail.com>
2025-10-20 19:28:01 +0200

    Style: Rename

--- NRLObstacleReporting/Database/ReportDTO.cs
+++ NRLObstacleReporting/Database/ReportDTO.cs
@@ -1,15 +1,15 @@
 ﻿namespace NRLObstacleReporting.Database;
 
-public class ReportDTO
+public class ReportDto
 {
     public int ReportId { get; set; }        
     public DateTime Dato { get; set; }
     
     //FK til statusId
     public int StatusId { get; set; }
-    public StatusDTO Status { get; set; } = null!;
+    public StatusDto Status { get; set; } = null!;
     
     //FK til pilotId
     public int PilotId { get; set; }
-    public PilotDTO? Pilot { get; set; } 
+    public PilotDto? Pilot { get; set; } 
 }
