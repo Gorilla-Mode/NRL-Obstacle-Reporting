8485e07d85cf196d15d3489a18b1e93267f29d2b
iverkroken <iverkroken@gmail.com>
2025-10-20 18:01:11 +0200

    Build: added ObstacleDTO.cs

---
+++ NRLObstacleReporting/Database/ObstacleDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace NRLObstacleReporting.Database;
+
+public class ObstacleDTO
+{
+    public bool IsDraft { get; set; } 
+    public int ObstacleId { get; set; }
+    
+    public int ObstacleTypeId { get; set; }               // FK til ObstacleTypeDatamodel
+    public ObstacleTypeDTO? ObstacleType { get; set; } // navigasjon
+    
+    public int ObstacleHeightMeter { get; set; }
+    
+    public string? GeometryGeoJson { get; set; }
+    
+    public string? ObstacleName { get; set; }
+    
+    public string? ObstacleDescription { get; set; }
+    
+    public bool? ObstacleIlluminated { get; set; }
+}
