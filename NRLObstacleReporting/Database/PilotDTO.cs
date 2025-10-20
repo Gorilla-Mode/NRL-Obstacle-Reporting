cd1d4d52699ecff5b7081d3e88c99391ddfba8ef
iverkroken <iverkroken@gmail.com>
2025-10-20 18:00:37 +0200

    Build: added PilotDTO.cs

---
+++ NRLObstacleReporting/Database/PilotDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace NRLObstacleReporting.Database;
+
+public class PilotDTO
+{
+    public int PilotId { get; set; }  //(PK)
+    public string? Organization { get; set; }
+    public string? Name { get; set; }
+}
