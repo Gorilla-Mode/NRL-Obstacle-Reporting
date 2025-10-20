13e917bcce1ce9ccb7e8a8a5867463f706d1ea03
iverkroken <iverkroken@gmail.com>
2025-10-20 17:57:20 +0200

    Build: added ReportDTO.cs

---
+++ NRLObstacleReporting/Database/ReportDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace NRLObstacleReporting.Database;
+
+public class ReportDTO
+{
+    public int ReportId { get; set; }        
+    public DateTime Dato { get; set; }
+    
+    //FK til statusId
+    public int StatusId { get; set; }
+    public StatusDTO Status { get; set; } = null!;
+    
+    //FK til pilotId
+    public int PilotId { get; set; }
+    public PilotDTO? Pilot { get; set; } 
+}
