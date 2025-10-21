0423e95fc0d057334302d4ec2fcfa31c7dfd718c
Gorilla-Mode <tobias_olsen02@hotmail.com>
2025-10-21 13:58:32 +0200

    database: powershell script to inject sql script and run on db container

---
+++ initdb.ps1
@@ -0,0 +1,21 @@
+﻿$scriptAbsolutePath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$sqlAbsolutePath = $scriptAbsolutePath+"/db.sql"
+$envAbsolutePath = $scriptAbsolutePath+"/.env"
+
+$envHash = @{}
+$envHash.Add('test', 'outtest')
+docker cp $sqlAbsolutePath db:/
+
+Get-Content $envAbsolutePath | foreach {
+    $variable, $value = $_.split('=')
+    if ([string]::IsNullOrWhiteSpace($variable) -or $variable.Contains('#')) 
+    {
+        #skips empty lines or comments
+        return
+    }
+    $envHash["$variable"] = "$value"
+}
+Write-Host $envHash['MYSQL_DATABASE']
+Write-Host $envHash['MYSQL_ROOT_PASSWORD']
+
+docker exec db sh -c "mariadb $($envHash['MYSQL_DATABASE']) -u root -p$($envHash['MYSQL_ROOT_PASSWORD']) <db.sql"
