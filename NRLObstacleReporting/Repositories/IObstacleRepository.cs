b868b64592b1f99d3a04f2a217d70330f235467c
Gorilla-Mode <tobias_olsen02@hotmail.com>
2025-10-23 18:13:23 +0200

    docs: added documentation to code

--- NRLObstacleReporting/Repositories/IObstacleRepository.cs
+++ NRLObstacleReporting/Repositories/IObstacleRepository.cs
@@ -5,8 +5,26 @@ namespace NRLObstacleReporting.Repositories;
 
 public interface IObstacleRepository
 {
+    /// <summary>
+    /// Writes fields part of step 1 to database
+    /// </summary>
+    /// <param name="data">extracted data is injected into sql</param>
+    /// <returns></returns>
+    /// 
     Task InsertStep1(ObstacleDto data);
+    
+    /// <summary>
+    /// Updates an entry with data from step 2
+    /// </summary>
+    /// <param name="data">extracted data is injected into sql. Data must include an id</param>
+    /// <returns></returns>
     Task InsertStep2(ObstacleDto data);
+    
+    /// <summary>
+    /// Updates an entry with data from step 3
+    /// </summary>
+    /// <param name="data">extracted data is injected into sql. Data must include an id</param>
+    /// <returns></returns>
     Task InsertStep3(ObstacleDto data);
     Task<IEnumerable<ObstacleDto>> GetAllObstacleData(); 
 }
