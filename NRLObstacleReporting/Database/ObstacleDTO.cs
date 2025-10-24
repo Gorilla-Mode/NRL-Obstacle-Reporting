1e469ed28db00319963c7c864a8eb7153c9fb3ec
Gorilla-Mode <tobias_olsen02@hotmail.com>
2025-10-25 01:06:28 +0200

    database: updated dto to match db col names, for functional mapping

--- NRLObstacleReporting/Database/ObstacleDTO.cs
+++ NRLObstacleReporting/Database/ObstacleDTO.cs
@@ -4,16 +4,17 @@ public class ObstacleDto
 {
     public int ObstacleId { get; set; }
     
-    public int ObstacleType { get; set; }               // FK til ObstacleTypeDatamodel
+    public int Type { get; set; }               // FK til ObstacleTypeDatamodel
     public ObstacleTypeDto? ObstacleTypeID { get; set; } // navigasjon
     
-    public int ObstacleHeightMeter { get; set; }
+    public int HeightMeter { get; set; }
     
     public string? GeometryGeoJson { get; set; }
     
-    public string? ObstacleName { get; set; }
+    public string? Name { get; set; }
     
-    public string? ObstacleDescription { get; set; }
+    public string? Description { get; set; }
     
-    public int ObstacleIlluminated { get; set; }
+    public int Illuminated { get; set; }
+    
 }
