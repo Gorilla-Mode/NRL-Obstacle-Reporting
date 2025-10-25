f7a709dcdb1a30b3b27b3d009c855ca59c1a9727
Gorilla-Mode <tobias_olsen02@hotmail.com>
2025-10-25 15:23:51 +0200

    database: interface and incomplete implementation of draft editig

---
+++ NRLObstacleReporting/Repositories/DraftRepository.cs
@@ -0,0 +1,23 @@
+﻿using NRLObstacleReporting.Database;
+
+namespace NRLObstacleReporting.Repositories;
+
+public class DraftRepository : RepositoryBase, IDraftRepository
+{
+    
+    //TODO: Implements these things
+    public Task EditDraft(int id, ObstacleDto data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SubmitDraft(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<ObstacleDto>> GetAllDrafts()
+    {
+        throw new NotImplementedException();
+    }
+}
