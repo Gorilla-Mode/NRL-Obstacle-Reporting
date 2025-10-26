f1d8d3732a0179ea792eb5ec18270f53bdc49604
sivert-svanes <siverss@uia.no>
2025-10-26 14:38:20 +0100

    feat: added AutoMapper profile folder

---
+++ NRLObstacleReporting/Profile/ObstacleProfile.cs
@@ -0,0 +1,12 @@
+﻿using NRLObstacleReporting.Database;
+using NRLObstacleReporting.Models;
+
+namespace NRLObstacleReporting.Profile;
+
+public class ObstacleProfile : AutoMapper.Profile
+{
+    public ObstacleProfile()
+    {
+        CreateMap<ObstacleDto, ObstacleCompleteModel>();
+    }
+}
