20d029fb0dd6769d086817a13778310c2525de39
Gorilla-Mode <tobias_olsen02@hotmail.com>
2025-10-28 16:17:29 +0100

    refactor: adde seperate map function without tracking

--- NRLObstacleReporting/wwwroot/js/MapScripts/GenerateMap.js
+++ NRLObstacleReporting/wwwroot/js/MapScripts/GenerateMap.js
@@ -12,6 +12,19 @@ function GenerateMap(zoom)
         minZoom: 4,
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+    
+    return map;
+}
+
+function GenerateMapGeolocate(zoom)
+{
+    var map = L.map('map').setView([58.14671, 7.9956], zoom);
+
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        minZoom: 4,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
     map.addControl(
         L.control.locate({
             locateOptions: {
