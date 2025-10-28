370a7b87b83866d8e0875fbeddc3d04b5049dd8b
Gorilla-Mode <tobias_olsen02@hotmail.com>
2025-10-28 13:27:40 +0100

    feat: function to track user position

--- NRLObstacleReporting/wwwroot/js/MapScripts/GenerateMap.js
+++ NRLObstacleReporting/wwwroot/js/MapScripts/GenerateMap.js
@@ -5,12 +5,18 @@
  */
 function GenerateMap()
 {
-     var map = L.map('map').setView([58.14671, 7.9956], 13);
+     var map = L.map('map').setView([58.14671, 7.9956], 17);
 
     L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
-    
+    map.addControl(
+        L.control.locate({
+            locateOptions: {
+                enableHighAccuracy: true
+            }
+        })
+    );
     return map;
 }
