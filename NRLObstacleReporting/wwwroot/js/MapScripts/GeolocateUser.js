4122fe3d861fa26d3653f7b9cab7f110a902fded
Gorilla-Mode <tobias_olsen02@hotmail.com>
2025-10-28 11:31:16 +0100

    feat: added script to locate user position

---
+++ NRLObstacleReporting/wwwroot/js/MapScripts/GeolocateUser.js
@@ -0,0 +1,39 @@
+﻿import {map} from "../../lib/leaflet/dist/leaflet-src";
+
+function GeolocateUser() {
+    if (navigator.geolocation) 
+    {
+        navigator.geolocation.getCurrentPosition(
+            function(position) 
+            {
+                let latitude = position.coords.latitude;
+                let longitude = position.coords.longitude;
+
+                L.marker([latitude, longitude]).addTo(map)
+                    .bindPopup('Your position')
+                    .openPopup();
+
+                map.setView([latitude, longitude], 13);
+            },
+            function(error) {
+                let mapContainer = document.getElementById('map');
+                switch (error.code) {
+                    case error.PERMISSION_DENIED:
+                        mapContainer.innerHTML = 'Access to geolocation was denied.';
+                        break;
+                    case error.POSITION_UNAVAILABLE:
+                        mapContainer.innerHTML = 'Location information is unavailable.';
+                        break;
+                    case error.TIMEOUT:
+                        mapContainer.innerHTML = 'The request to get user location timed out.';
+                        break;
+                    default:
+                        mapContainer.innerHTML = 'An error occurred while retrieving geolocation.';
+                        break;
+                }
+            }
+        );
+    } else {
+        document.getElementById('map').innerHTML = 'Geolocation is not supported by your browser.';
+    }
+}
