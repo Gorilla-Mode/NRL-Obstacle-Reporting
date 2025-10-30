6ae6f93f43f35bc25c2fe2a8e74f2879d3364659
Gorilla-Mode <tobias_olsen02@hotmail.com>
2025-10-30 13:59:43 +0100

    database: added repo to get all drafts from db

--- NRLObstacleReporting/Repositories/DraftRepository.cs
+++ NRLObstacleReporting/Repositories/DraftRepository.cs
@@ -1,4 +1,6 @@
-﻿using NRLObstacleReporting.Database;
+﻿using Dapper;
+using NRLObstacleReporting.Database;
+using NRLObstacleReporting.Models;
 
 namespace NRLObstacleReporting.Repositories;
 
@@ -6,18 +8,22 @@ public class DraftRepository : RepositoryBase, IDraftRepository
 {
     
     //TODO: Implements these things
-    public Task EditDraft(int id, ObstacleDto data)
+    public async Task EditDraft(int id, ObstacleDto data)
     {
         throw new NotImplementedException();
     }
 
-    public Task SubmitDraft(int id)
+    public async Task SubmitDraft(int id)
     {
         throw new NotImplementedException();
     }
 
-    public Task<IEnumerable<ObstacleDto>> GetAllDrafts()
+    public async Task<IEnumerable<ObstacleDto>> GetAllDrafts()
     {
-        throw new NotImplementedException();
+        using var connection = CreateConnection();
+        const int statusId = (int)ObstacleCompleteModel.ObstacleStatus.Draft;
+        var sql = $"select * from Obstacle where Status = {statusId}";
+        
+        return await connection.QueryAsync<ObstacleDto>(sql);
     }
 }
