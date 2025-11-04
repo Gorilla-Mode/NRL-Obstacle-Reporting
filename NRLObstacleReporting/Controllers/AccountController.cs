6b15c188ccb2cb8b3017c8874b926cf670958ee4
Gorilla-Mode <tobias_olsen02@hotmail.com>
2025-11-04 17:54:11 +0100

    fix: moved log out button to view, updated controller to correct view

--- NRLObstacleReporting/Controllers/AccountController.cs
+++ NRLObstacleReporting/Controllers/AccountController.cs
@@ -117,7 +117,7 @@ namespace NRLObstacleReporting.Controllers
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation(4, "User logged out.");
-            return RedirectToAction(nameof(AccountController.Login), "Login");
+            return RedirectToAction(nameof(AccountController.Login), "Account");
         }
 
         //
