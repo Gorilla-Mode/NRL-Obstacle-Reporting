23a772169379545226df4d2cf0bb472e73000976
Gorilla-Mode <tobias_olsen02@hotmail.com>
2025-11-22 12:14:54 +0100

    database: added data access to view_userrole view

---
+++ NRLObstacleReporting/Database/ViewUserRoleDTO.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NRLObstacleReporting.Database;
+
+public class ViewUserRoleDto : IdentityUser
+{
+    public required string RoleId { get; set; }
+}
