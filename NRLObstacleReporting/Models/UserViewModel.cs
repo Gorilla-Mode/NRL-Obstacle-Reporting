0e556cc861ae0fd8c075ed6698adf590addc663b
Gorilla-Mode <tobias_olsen02@hotmail.com>
2025-11-22 12:16:22 +0100

    refactor: updated to work with repo implementation

--- NRLObstacleReporting/Models/UserViewModel.cs
+++ NRLObstacleReporting/Models/UserViewModel.cs
@@ -6,5 +6,5 @@ public class UserViewModel
     public string? UserName { get; set; }
     public string? Email { get; set; }
     public string? PhoneNumber { get; set; }
-    public string? Role { get; set; }
+    public string? RoleId { get; set; }
 }
