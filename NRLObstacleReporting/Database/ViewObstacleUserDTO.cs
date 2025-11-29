1ffb287d6f3fc7385ebfc01fe3bc5f9ef96deda7
Gorilla-Mode <tobias_olsen02@hotmail.com>
2025-11-29 16:12:21 +0100

    refactor: changed dtos to records

--- NRLObstacleReporting/Database/ViewObstacleUserDTO.cs
+++ NRLObstacleReporting/Database/ViewObstacleUserDTO.cs
@@ -5,27 +5,9 @@ namespace NRLObstacleReporting.Database;
 /// <summary>
 /// Datatransfer object targeting view view_ObstacleUser
 /// </summary>
-public class ViewObstacleUserDto : IdentityUser
+public record ViewObstacleUserDto : ObstacleDto
 {
-    public required string ObstacleId { get; init; }
-    
-    public required string UserId { get; init; }
-    
-    public int Type { get; set; }               // FK to ObstacleTypeDatamodel
-    public int Status { get; set; }               // FK to Status
-    public int Marking { get; set; }               //FK to Marking
-    public int HeightMeter { get; init; }
-    
-    public string? GeometryGeoJson { get; init; }
-    
-    public string? Name { get; init; }
-    
-    public string? Description { get; init; }
-    
-    public int Illuminated { get; init; }
-    
-    public DateTime CreationTime { get; init; }
-    
-    public DateTime UpdatedTime { get; init; }
-    
+    public string? UserName { get; init; }
+    public string? Email { get; init; }
+    public string? PhoneNumber { get; init; }
 }
--- NRLObstacleReporting/Database/ViewUserRoleDTO.cs
+++ NRLObstacleReporting/Database/ViewUserRoleDTO.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace NRLObstacleReporting.Database;
+﻿namespace NRLObstacleReporting.Database;
 
 /// <summary>
 /// Data-transfer object targeting view view_UserRole
 /// </summary>
-public class ViewUserRoleDto : IdentityUser
+public record ViewUserRoleDto 
 {
     public required string RoleId { get; init; }
     public required string UserId { get; init; }
+    
+    public string? UserName { get; init; }
+    public string? Email { get; init; }
+    public string? PhoneNumber { get; init; }
 }
