45f3ea369ff291ee23dc6d54173aa152c29b91f5
Gorilla-Mode <tobias_olsen02@hotmail.com>
2025-12-03 00:13:41 +0100

    test: added test coverage for draft controller

--- NRLObstacleReporting.UnitTests/Controllers/DraftControllerTest.cs
+++ NRLObstacleReporting.UnitTests/Controllers/DraftControllerTest.cs
@@ -66,7 +66,7 @@ public class DraftControllerTest
     /// returns a view result with the appropriate output when invoked.
     /// </summary>
     [Fact]
-    public void PilotDrafts_ReturnsPilotDraftsView()
+    public void PilotDraftsGET_ReturnsPilotDraftsView()
     {
         //arrange
         var controller = CreateDraftController();
@@ -80,33 +80,49 @@ public class DraftControllerTest
     }
 
     /// <summary>
-    /// Validates that the <see cref="DraftController.PilotDrafts"/> method throws an <see cref="InvalidOperationException"/>
-    /// when the user ID cannot be retrieved.
+    /// Tests that methods in <see cref="DraftController"/> throw appropriate exceptions when accessed by an invalid or unauthorized user.
     /// </summary>
-    /// <exception cref="InvalidOperationException">
-    /// Thrown when the current user ID is null or cannot be determined.
-    /// </exception>
-    /// <exception cref="AggregateException">
-    /// Thrown when exception is asserted before aggregate is unwrapped. Not strictly a necessary test
-    /// </exception>
+    /// <remarks>
+    /// This test verifies that each method in <see cref="DraftController"/> correctly throws an <see cref="InvalidOperationException"/>
+    /// And an <see cref="AggregateException"/> which is not strictly necessary, but it confirms it's a task.
+    /// </remarks>
     [Fact]
-    public void PilotDrafts_InvalidUserIdThrowsInvalidOperationException()
+    public void DraftController_MethodsThrowOnInvalidUser()
     {
         //arrange
         var controller = CreateDraftController();
+        
         controller.ControllerContext = new ControllerContext
         {
             HttpContext = Substitute.For<HttpContext>()
         };
         
         // Act
-        //DO NOT MESS. Defers call to assertion to avoid internal throw outside assert context 
-        var unwrappedResult = () => controller.PilotDrafts().GetAwaiter().GetResult(); //Test the actual exception in controller
-        var wrappedResult = () => controller.PilotDrafts().Result;
+        //DO NOT MESS. Defers call to assertion to avoid internal throw outside assert context
+        var pilotDraftUnwrappedResult = () => controller.PilotDrafts().GetAwaiter().GetResult(); //Test the actual exception in controller
+        var pilotDraftWrappedResult = () => controller.PilotDrafts().Result;
+        
+        var editDraftUnwrappedResult = () => controller.EditDraft("1").GetAwaiter().GetResult(); //Test the actual exception in controller
+        var editDraftWrappedResult = () => controller.EditDraft("1").Result;
+        
+        var saveEditedDraftUnwrappedResult = () => controller.SaveEditedDraft(Substitute.For<ObstacleCompleteModel>()).GetAwaiter().GetResult(); //Test the actual exception in controller
+        var saveEditedDraftWrappedResult = () => controller.SaveEditedDraft(Substitute.For<ObstacleCompleteModel>()).Result;
+        
+        var submitDraftUnwrappedResult = () => controller.SubmitDraft(Substitute.For<ObstacleCompleteModel>()).GetAwaiter().GetResult(); //Test the actual exception in controller
+        var submitDraftWrappedResult = () => controller.SubmitDraft(Substitute.For<ObstacleCompleteModel>()).Result;
         
         //assert
-        Assert.Throws<InvalidOperationException>(unwrappedResult);
-        Assert.Throws<AggregateException>(wrappedResult);
+        Assert.Throws<InvalidOperationException>(pilotDraftUnwrappedResult);
+        Assert.Throws<AggregateException>(pilotDraftWrappedResult);
+        
+        Assert.Throws<InvalidOperationException>(editDraftUnwrappedResult);
+        Assert.Throws<AggregateException>(editDraftWrappedResult);
+        
+        Assert.Throws<InvalidOperationException>(saveEditedDraftUnwrappedResult);
+        Assert.Throws<AggregateException>(saveEditedDraftWrappedResult);
+        
+        Assert.Throws<InvalidOperationException>(submitDraftUnwrappedResult);
+        Assert.Throws<AggregateException>(submitDraftWrappedResult);
     }
 
     /// <summary>
@@ -119,7 +135,7 @@ public class DraftControllerTest
     /// assert that the method returns proper view
     /// </remarks>
     [Fact]
-    public void PilotDrafts_SetsViewDataWithDrafts()
+    public void PilotDraftsGET_SetsViewDataWithDrafts()
     {
         // Arrange
         var controller = CreateDraftController();
@@ -145,8 +161,15 @@ public class DraftControllerTest
         
         var draftViewModel = new List<ObstacleCompleteModel>
         {
-            new ObstacleCompleteModel { ObstacleId = "1", UserId = userId },
-            new ObstacleCompleteModel { ObstacleId = "2", UserId = userId }
+            new ObstacleCompleteModel
+                { ObstacleId = "1",
+                    UserId = userId 
+                },
+            new ObstacleCompleteModel 
+                { 
+                    ObstacleId = "2",
+                    UserId = userId 
+                }
         };
         _signInManager.UserManager.GetUserId(Arg.Any<ClaimsPrincipal>()).Returns(userId);
         _mapper.Map<IEnumerable<ObstacleCompleteModel>>(draftDto).Returns(draftViewModel);
@@ -173,7 +196,7 @@ public class DraftControllerTest
     /// and asserts that the result is the expected view.
     /// </remarks>
     [Fact]
-    public void SubmitDraft_InvalidModelStateReturnsSubmitDraftView()
+    public void SubmitDraftPOST_InvalidModelStateReturnsSubmitDraftView()
     {
         //arrange
         var controller = CreateDraftController();
@@ -190,5 +213,121 @@ public class DraftControllerTest
     }
     
     
-}
+    /// <summary>
+    /// Tests that the <see cref="DraftController.SubmitDraft(ObstacleCompleteModel)"/> method
+    /// redirects to the PilotDrafts action when the provided model state is valid.
+    /// </summary>
+    /// <remarks>
+    /// Provides a valid model state, invokes the method, and checks that the result is a redirect to PilotDrafts.
+    /// </remarks>
+    [Fact]
+    public void SubmitDraftPOST_ValidModelStateRedirectsToPilotDrafts()
+    {
+        //arrange
+        var controller = CreateDraftController();
+        
+        const string expectedAction = "PilotDrafts";
+        var model = Substitute.For<ObstacleCompleteModel>(); // Creates substitute model for method
+
+        //act
+        var result = controller.SubmitDraft(model);
+        var redirectResult = result.Result as RedirectToActionResult;
+
+        //assert
+        Assert.NotNull(result);
+        Assert.NotNull(redirectResult);
+        Assert.Equal(expectedAction, redirectResult!.ActionName);
+    }
+
+    /// <summary>
+    /// Tests the behavior of the EditDraft method in the <see cref="DraftController"/> when provided with a valid draft ID
+    /// for an authenticated user.
+    /// Ensures that the method returns the EditDraft view with the expected model.
+    /// </summary>
+    [Fact]
+    public void EditDraftGET_ReturnsEditDraftViewWithModel()
+    {
+        // Arrange
+        var controller = CreateDraftController();
+        
+        const string draftId = "test-draft-id";
+        const string userId = "test-user-id";
+
+        var draftDto = new ObstacleDto
+        {
+            ObstacleId = draftId,
+            UserId = userId,
+            Status = (int)ObstacleCompleteModel.ObstacleStatus.Draft
+        };
+
+        var draftViewModel = new ObstacleCompleteModel
+        {
+            ObstacleId = draftId,
+            UserId = userId,
+            Status = (int)ObstacleCompleteModel.ObstacleStatus.Draft
+        };
+        
+        _signInManager.UserManager.GetUserId(Arg.Any<ClaimsPrincipal>()).Returns(userId);
+        _draftRepository.GetDraftByIdAsync(draftId, userId).Returns(draftDto);
+        _mapper.Map<ObstacleCompleteModel>(draftDto).Returns(draftViewModel);
+
+        // Act
+        var result = controller.EditDraft(draftId).Result;
+        var viewResult = result as ViewResult;
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.NotNull(viewResult);
+        Assert.Null(viewResult!.ViewName);
+        Assert.Equal(draftViewModel, viewResult.Model);
+    }
     
+    /// <summary>
+    /// Tests that the <see cref="DraftController.SaveEditedDraft(ObstacleCompleteModel)"/> method
+    /// redirects to the PilotDrafts action when the provided model state is valid.
+    /// </summary>
+    [Fact]
+    public void SaveEditedDraftPOST_RedirectsToPilotDrafts()
+    {
+        // Arrange
+        var controller = CreateDraftController();
+        
+        const string expectedAction = "PilotDrafts";
+        var model = Substitute.For<ObstacleCompleteModel>(); 
+
+        // Act
+        var result = controller.SaveEditedDraft(model);
+        var redirectResult = result.Result as RedirectToActionResult;
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.NotNull(redirectResult);
+        Assert.Equal(expectedAction, redirectResult!.ActionName);
+    }
+
+    /// <summary>
+    /// Tests that the SaveEditedDraft action method of <see cref="DraftController"/>
+    /// returns the EditDraft view when the model state is invalid. Confimring that a user is able so save an obstacle
+    /// as a draft even if model state is invalid
+    /// </summary>
+    [Fact]
+    public void SaveEditedDraftPOST_SavingDraftAllowsInvalidModelState()
+    {
+        // Arrange
+        var controller = CreateDraftController();
+
+        const string expectedAction = "PilotDrafts";
+        var model = Substitute.For<ObstacleCompleteModel>();
+        
+        controller.ModelState.AddModelError("Error", "Invalid model state");
+
+        // Act
+        var result = controller.SaveEditedDraft(model).Result;
+        var redirectResult = result as RedirectToActionResult;
+        
+        // Assert
+        Assert.NotNull(result);
+        Assert.NotNull(redirectResult);
+        Assert.Equal(expectedAction, redirectResult!.ActionName);
+    }
+}
