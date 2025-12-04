07af6e0254a292006276a2d423374e6b1e52b778
Gorilla-Mode <tobias_olsen02@hotmail.com>
2025-12-05 00:04:08 +0100

    refactor: made testable

--- NRLObstacleReporting/Repositories/RegistrarRepository.cs
+++ NRLObstacleReporting/Repositories/RegistrarRepository.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using Dapper;
 using System.Globalization;
 using NRLObstacleReporting.Database;
@@ -7,51 +8,59 @@ namespace NRLObstacleReporting.Repositories;
 
 public sealed class RegistrarRepository : RepositoryBase, IRegistrarRepository
 {
+    private readonly IDbConnection _connection;
+
+    public RegistrarRepository()
+    {
+        _connection = CreateConnection();
+    }
+
+    public RegistrarRepository(IDbConnection mockconnection)
+    {
+        _connection = mockconnection;
+    }
+
+
     /// <inheritdoc/>
     public async Task<IEnumerable<ObstacleDto>> GetAllSubmittedObstaclesAsync()
     {
-        
-        using var connection = CreateConnection();
         var sql = @$"SELECT * 
                      FROM Obstacle 
                      WHERE Status <> {(int)ObstacleCompleteModel.ObstacleStatus.Draft}";
-        
-        return await connection.QueryAsync<ObstacleDto>(sql);
+
+        return await _connection.QueryAsync<ObstacleDto>(sql);
     }
 
     /// <inheritdoc/>
     public async Task<ViewObstacleUserDto> GetSubmittedObstacleByIdAsync(string id)
     {
-        using var connection = CreateConnection();
         var sql = $@"SELECT * 
                     FROM view_ObstacleUser
                     WHERE ObstacleID = @id AND Status <> {(int)ObstacleCompleteModel.ObstacleStatus.Draft}";
 
-        return await connection.QuerySingleAsync<ViewObstacleUserDto>(sql, new { Id = id });
+        return await _connection.QuerySingleAsync<ViewObstacleUserDto>(sql, new { Id = id });
     }
 
     /// <inheritdoc/>
     public async Task UpdateObstacleStatusAsync(ObstacleDto data)
     {
-        using var connection = CreateConnection();
         var sql = @"UPDATE Obstacle 
                     SET Status = @Status
                     WHERE ObstacleID = @ObstacleId";
-        await connection.ExecuteAsync(sql, data);
+        await _connection.ExecuteAsync(sql, data);
     }
 
     /// <inheritdoc/>
     public async Task<IList<ObstacleDto>> GetObstaclesByStatusAsync(ObstacleCompleteModel.ObstacleStatus[] status)
     {
-        using var connection = CreateConnection();
         string statusList = string.Join(", ", status.Select(s => (int)s));
-        
+
         var sql = $@"SELECT *
                 FROM Obstacle
                 WHERE Status IN ({statusList})";
-        
-        var queryResult = await connection.QueryAsync<ObstacleDto>(sql);
-        
+
+        var queryResult = await _connection.QueryAsync<ObstacleDto>(sql);
+
         return queryResult.ToList();
     }
 
@@ -61,14 +70,11 @@ public sealed class RegistrarRepository : RepositoryBase, IRegistrarRepository
         ObstacleCompleteModel.ObstacleMarking[] markings, DateOnly dateStart, DateOnly dateEnd)
     {
         //TODO: Parameterize the variables
-        
-        using var connection = CreateConnection();
-        
         string sql = $@"SELECT * 
                         FROM Obstacle 
                         WHERE 1=1
                         AND Status <> {(int)ObstacleCompleteModel.ObstacleStatus.Draft}"; //base sql returns whole table
-        
+
         if (status.Length != 0)
         {
             string statusList = string.Join(", ", status.Select(s => (int)s));
@@ -79,34 +85,34 @@ public sealed class RegistrarRepository : RepositoryBase, IRegistrarRepository
         if (type.Length != 0)
         {
             string typeList = string.Join(", ", type.Select(t => (int)t));
-            
+
             sql += $" AND Type IN ({typeList})";
         }
-        
+
         if (illuminations.Length != 0)
         {
             string illuminationList = string.Join(", ", illuminations.Select(m => (int)m));
-            
+
             sql += $" AND Illuminated IN ({illuminationList})";
         }
-        
+
         if (markings.Length != 0)
         {
             string markingList = string.Join(", ", markings.Select(i => (int)i));
-            
+
             sql += $" AND Marking IN ({markingList})";
         }
 
         if (dateStart.ToString() != String.Empty && dateEnd.ToString() != String.Empty)
         {
-           string formattedDateStart = dateStart.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
-           string formattedDateEnd = dateEnd.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
-           
+            string formattedDateStart = dateStart.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
+            string formattedDateEnd = dateEnd.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
+
             sql += $" AND CreationTime BETWEEN '{formattedDateStart}' AND '{formattedDateEnd}'";
         }
-        
-        var queryResult = await connection.QueryAsync<ObstacleDto>(sql);
-        
+
+        var queryResult = await _connection.QueryAsync<ObstacleDto>(sql);
+
         return queryResult.ToList();
     }
 }
