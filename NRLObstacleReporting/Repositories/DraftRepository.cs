f8c76cc75be9c4bd0dc4c043350ba42d48bc543a
Gorilla-Mode <tobias_olsen02@hotmail.com>
2025-12-04 19:02:18 +0100

    refactor: made testable

--- NRLObstacleReporting/Repositories/DraftRepository.cs
+++ NRLObstacleReporting/Repositories/DraftRepository.cs
@@ -1,4 +1,5 @@
-﻿using Dapper;
+﻿using System.Data;
+using Dapper;
 using NRLObstacleReporting.Database;
 using NRLObstacleReporting.Models;
 
@@ -6,56 +7,62 @@ namespace NRLObstacleReporting.Repositories;
 
 public sealed class DraftRepository : RepositoryBase, IDraftRepository
 {
+    private readonly IDbConnection _connection;
+
+    public DraftRepository()
+    {
+        _connection = CreateConnection();
+    }
+
+    public DraftRepository(IDbConnection mockconnection)
+    {
+        _connection = mockconnection;
+    }
+
     /// <inheritdoc/>
     public async Task EditDraftAsync(ObstacleDto data)
     {
-        using var connection = CreateConnection();
-        
         var sql = @"UPDATE Obstacle 
                     SET Heightmeter = @HeightMeter, GeometryGeoJson = @GeometryGeoJson, Name = @Name, 
                         Description = @Description, Illuminated = @Illuminated, Type = @Type, Status = @Status,
                         Marking = @Marking, UpdatedTime = @UpdatedTime
                     WHERE ObstacleID = @ObstacleId AND UserId = @UserId";
-        
-        await connection.ExecuteAsync(sql, data);
+
+        await _connection.ExecuteAsync(sql, data);
     }
 
     /// <inheritdoc/>
     public async Task SubmitDraftAsync(ObstacleDto data)
     {
-        using var connection = CreateConnection();
-        
         const int statusId = (int)ObstacleCompleteModel.ObstacleStatus.Pending;
-        
+
         var sql = @$"UPDATE Obstacle
                      SET Status = {statusId} 
                      WHERE ObstacleID = @ObstacleID AND UserId = @UserId";
-        
-        await connection.ExecuteAsync(sql, data);
+
+        await _connection.ExecuteAsync(sql, data);
     }
 
     /// <inheritdoc/>
     public async Task<IEnumerable<ObstacleDto>> GetAllDraftsAsync(string userId)
     {
-        using var connection = CreateConnection();
         const int statusId = (int)ObstacleCompleteModel.ObstacleStatus.Draft;
-        
+
         var sql = @$"SELECT * 
                      FROM Obstacle 
                      WHERE Status = {statusId} AND UserId = @userId
                      ORDER BY CreationTime DESC";
-        
-        return await connection.QueryAsync<ObstacleDto>(sql, new { UserId = userId });
+
+        return await _connection.QueryAsync<ObstacleDto>(sql, new { UserId = userId });
     }
 
     /// <inheritdoc/>
     public async Task<ObstacleDto> GetDraftByIdAsync(string obstacleId, string userId)
     {
-        using var connection = CreateConnection();
         var sql = @"SELECT *
                     FROM Obstacle
                     WHERE ObstacleID = @obstacleId AND UserId = @userId";
 
-        return await connection.QuerySingleAsync<ObstacleDto>(sql, new { UserId = userId, ObstacleId = obstacleId });
+        return await _connection.QuerySingleAsync<ObstacleDto>(sql, new { UserId = userId, ObstacleId = obstacleId });
     }
 }
